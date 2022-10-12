61ebdef8a61f56c53f1332b21d6adde003196f1d
Nick Pavlovic <nickpavlovic@icloud.com>
2022-10-12 17:20:22 -0400

    completed render HTML and save to file

--- index.js
+++ index.js
@@ -24,6 +24,14 @@ const Manager = require("./lib/Manager");
 const Engineer = require("./lib/Engineer");
 const Intern = require("./lib/Intern");
 const inquirer = require("inquirer");
+const fs = require('fs');
+
+const filename="./dist/teamprofile.html";
+
+const managers = []; // array for managers
+const engineers = []; //array for engineers
+const interns = []; //array for interns
+
 
 const ManagerQuestions = [      
     {
@@ -62,7 +70,7 @@ const ManagerQuestions = [
       {
         type: 'input',
         message: "Enter team manager's office number:",
-        name: 'managerOfficeNumber',
+        name: 'officeNumber',
         validate(text) {
           if (text==="") {
             return `Manager must have an office number.`;
@@ -145,7 +153,7 @@ const ManagerQuestions = [
       {
         type: 'input',
         message: "Enter intern's email address:",
-        name: 'InternEmail',
+        name: 'internEmail',
         validate(text) {
           if (text==="") {
             return `Intern must have an email address.`;
@@ -170,7 +178,8 @@ function addEngineer() {
     console.log("\n-----Add an Engineer-----\n");
     inquirer.prompt(EngineerQuestions).then((answers)=>{
         console.log("\n-----Engineer Added-----\n");
-        console.log(answers);
+        const newEngineer = new Engineer(answers.engineerName,answers.engineerID,answers.engineerEmail,answers.engineerGithub);
+        engineers.push(newEngineer);
         quit();
     });
 }
@@ -179,7 +188,8 @@ function addIntern() {
     console.log("\n-----Add an Intern-----\n");
     inquirer.prompt(InternQuestions).then((answers)=>{
         console.log("\n-----Intern Added----\n");
-        console.log(answers);
+        const newIntern = new Intern(answers.internName,answers.internID,answers.internEmail,answers.internSchool);
+        interns.push(newIntern);
         quit();
     });
 }
@@ -192,7 +202,6 @@ function addToTeam() {
       message: 'Select a team member to add:',
       choices: ['Engineer','Intern'],
     }]).then((answers)=>{
-        console.log(answers);
         switch (answers.addTeamMember) {
             case 'Engineer': addEngineer();
                 break;
@@ -200,6 +209,83 @@ function addToTeam() {
                 break;
         }
     });
+}
+
+function renderHTML() {
+
+const topHTML= `<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/css/bootstrap.min.css" integrity="sha384-TX8t27EcRE3e/ihU7zmQxVncDAy5uIKz4rEkgIXeMed4M0jlfIDPvg6uqKI2xXr2" crossorigin="anonymous">
+    <title>Document</title>
+</head>
+<body style="background-color: white;">
+    <div class="jumbotron text-center text-white bg-primary jumbotron-fluid">
+        <div class="container">
+          <h1 class="display-4">Team Profile</h1>
+        </div>
+    </div>
+    <div class="container">
+      <div class="card-deck">`;
+
+
+const bottomHTML=
+`     </div>
+</body>
+</html>`;
+
+let dynamicHTML='';
+
+managers.forEach((item)=> {
+
+           dynamicHTML+=`
+            <div class="card border-dark mb-3" style="min-width: 20rem; max-width: 20rem;box-shadow: 5px 5px #888888;">
+            <div class="card-header text-white bg-primary" style="font-size: 1.5rem; font-weight: bold;">${item.getName()}<br/>☕️ ${item.getRole()}</div> 
+            <div class="card-body text-dark">
+              <ul class="list-group"style="width: 17rem;">
+                <li class="list-group-item">ID: <span>${item.getID()}</span></li>
+                <li class="list-group-item">Email: <a href="mailto:${item.getEmail()}">${item.getEmail()}</a></li>
+                <li class="list-group-item">Office: <span>${item.officeNumber}</span></li>
+            </ul>
+            </div>
+            </div>\n`;  
+  });
+
+  engineers.forEach((item)=> {
+          
+    dynamicHTML+=`
+     <div class="card border-dark mb-3" style="min-width: 20rem; max-width: 20rem;box-shadow: 5px 5px #888888;">
+     <div class="card-header text-white bg-primary" style="font-size: 1.5rem; font-weight: bold;">${item.getName()}<br/>🥽 ${item.getRole()}</div> 
+     <div class="card-body text-dark">
+       <ul class="list-group"style="width: 17rem;">
+       <li class="list-group-item">ID: <span>${item.getID()}</span></li>
+       <li class="list-group-item">Email: <a href="mailto:${item.getEmail()}">${item.getEmail()}</a></li>
+       <li class="list-group-item">Github User: <a href="https://github.com/${item.getGitHub()}" target="_blank">${item.getGitHub()}</a></li>
+     </ul>
+     </div>
+     </div>\n`;  
+
+});
+     interns.forEach((item)=> {
+          
+      dynamicHTML+=`
+       <div class="card border-dark mb-3" style="min-width: 20rem; max-width: 20rem;box-shadow: 5px 5px #888888;">
+       <div class="card-header text-white bg-primary" style="font-size: 1.5rem; font-weight: bold;">${item.getName()}<br/>🎓 ${item.getRole()}</div> 
+       <div class="card-body text-dark">
+         <ul class="list-group"style="width: 17rem;">
+         <li class="list-group-item">ID: <span>${item.getID()}</span></li>
+         <li class="list-group-item">Email: <a href="mailto:${item.getEmail()}">${item.getEmail()}</a></li>
+         <li class="list-group-item">School: <span>${item.getSchool()}</span></li>
+       </ul>
+       </div>
+       </div>\n`;  
+});
+
+ return topHTML + '\n' +dynamicHTML + '\n' + bottomHTML;
+
 }
 
 function quit() {
@@ -216,7 +302,10 @@ function quit() {
             if (answers.askAgain) {
                 addToTeam();
             } else {
-                console.log('\nExiting program!\n');
+                fs.writeFile(filename, renderHTML(), (error) =>
+                error 
+                ? console.error(error) 
+                : console.log('Saving HTML and exiting program!'));
             }
 
 
@@ -224,14 +313,13 @@ function quit() {
 }
 
 function init() {
-// const John = new Manager('John',1,'john@company.com',200);
-// const Sam = new Engineer('Sam',2,'sam@company.com','sam9999');
-// const Emily = new Intern('Emily',3,'emily@company.com','Mothercraft');
-
-// console.log(`${John.getRole()}: ${John.getName()}, ${John.getID()}, ${John.getEmail()}, Office: ${John.officeNumber}`);
 
 inquirer.prompt(ManagerQuestions).then((answers)=>{
-        addToTeam();
+        
+    // TODO - init manager object and add to array
+    const newManager = new Manager(answers.managerName,answers.managerID,answers.managerEmail,answers.officeNumber);
+    managers.push(newManager);
+    addToTeam();
 });
 
 }
